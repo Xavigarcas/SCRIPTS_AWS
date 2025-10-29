fcab4d9e9e0d4388e56f8c4e713d1fb2e55cc44e
Xavi <xavgarcas@alu.edu.gva.es>
2025-10-29 19:19:13 +0100

    solo vpc funcional

--- script1vpc.sh
+++ script1vpc.sh
@@ -1,3 +1,16 @@
-aws ec2 create-vpc --cidr-block 192.168.0.0/24  --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=xaviVPC}]' --query Vpc.VpcId --output text
+#Creo la vpc y devuelvo su id
+VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.1.0/24  \
+    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=nubexavi}]' \
+    --query Vpc.VpcId --output text)
 
-ws ec2 create-subnet --vpc-id nombrevpc --cidr-block 192.168.0.0/28 --availability-zone us-east-1a --query Subnet.SubnetId --output text
+echo $VPC_ID
+
+#Habilitar el dns en el vpc
+aws ec2 modify-vpc-attribute \
+    --vpc-id $VPC_ID \
+    --enable-dns-hostnames "{\"Value\":true}"
+
+#Creo la subnet y devuelvo su id
+#SUBNET_ID=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block 192.168.0.0/28 \
+#    --availability-zone us-east-1a --query Subnet.SubnetId --output text)
+#echo $SUBNET_ID
