093cd1db9cb605efa68416013dbf96de6cbc7343
Xavi <xavgarcas@alu.edu.gva.es>
2025-11-05 18:34:49 +0100

    Subiendo script1 de vpc,subredes,gs y ec2, ademas de la nueva organizacion de carpetas

--- REDES/script1vpc.sh
+++ REDES/script1vpc.sh
@@ -1,6 +1,6 @@
 #Creo la vpc y devuelvo su id
-VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.1.0/24  \
-    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=nubexavi}]' \
+VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.2.0/24  \
+    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=nubexavi2}]' \
     --query Vpc.VpcId --output text)
 
 echo $VPC_ID
@@ -13,8 +13,8 @@ aws ec2 modify-vpc-attribute \
 #Creo la subnet y devuelvo su id
 SUBNET_ID=$(aws ec2 create-subnet \
     --vpc-id $VPC_ID \
-    --cidr-block 192.168.1.0/28 \
-    --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=misubredXAVI2}]' \
+    --cidr-block 192.168.2.0/28 \
+    --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=misubredXAVI3}]' \
     --query Subnet.SubnetId --output text)
 
 echo $SUBNET_ID
@@ -26,19 +26,23 @@ aws ec2 modify-subnet-attribute \
   --map-public-ip-on-launch
 
 #Creando grupo de seguridad
-$SG_ID=$(aws ec2 create-security-group --group-name SGVPC --description "Mi grupo de seguridad para abrir el puerto 80" --output text)
+SG_ID=$(aws ec2 create-security-group \
+  --group-name SGVPC \
+  --description "Mi grupo de seguridad para abrir el puerto 80" \
+  --vpc-id $VPC_ID \
+  --query GroupId --output text)
 
 aws ec2 authorize-security-group-ingress \
     --group-id $SG_ID \
-    --protocol tcp \
-    --port 80 \
-    --cidr 0.0.0.0/24
-
+    --ip-permissions '[{"IpProtocol":"tcp","FromPort":22,"ToPort":22,"IpRanges":[{"CidrIp":"0.0.0.0/0", "Description": "Allow ssh"}]}]' 
+#    --protocol tcp \
+#    --port 80 \
+#    --cidr 0.0.0.0/0 > /dev/null
 
 #Creando instancia ec2
 EC2_ID=$(aws ec2 run-instances \
     --image-id ami-0360c520857e3138f \
-    --instance-type t3.micro \
+    --instance-type t2.micro \
     --subnet-id $SUBNET_ID \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=miec2}]' \
     --key-name vockey \
@@ -48,3 +52,4 @@ EC2_ID=$(aws ec2 run-instances \
 
 sleep 15
 echo $EC2_ID
+
